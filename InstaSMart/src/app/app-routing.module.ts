51bbf1720a1aabc5c48f78d8fd4c05d09ddef95d
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2023-01-13 11:21:23 +0530

    Add files via upload

---
+++ InstaSMart/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { CartListComponent } from './product/cart/cart-list.component';
+import { ProductAddComponent } from './product/product-add/product-add.component';
+
+const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'home'},
+  {path:'home',component:HomePageComponent},
+  {path:'nav',component:NavbarComponent},
+  {path:'contact us',component:ContactUsComponent},
+  {path:'about us',component:AboutUsComponent},
+  {path:'products',
+    loadChildren:()=>import('./product/product.module').then((m)=>m.ProductModule)
+    //component:ProductComponent,
+    // canActivate: [AuthGuard],
+    // children:[{path:'addProduct',component:ProductAddComponent}]
+  },
+  {path:'cart',component:CartListComponent},
+  {path:'addProduct',component:ProductAddComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ InstaSMart/src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import { InMemoryDbEventService } from 'shared/inmemoryeventdbservice';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppEffects } from './app.effects';
+import { CartListComponent } from './product/cart/cart-list.component';
+import { ProductAddComponent } from './product/product-add/product-add.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    PageNotFoundComponent,
+    HomePageComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    CartListComponent,
+    ProductListComponent,
+    ProductAddComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    CommonModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDbEventService),
+    StoreModule.forRoot({}),
+    //have to create AppEffects
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
