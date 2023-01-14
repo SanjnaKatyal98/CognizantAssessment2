991b964f088280db0cb7d235ab7183642000774f
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2023-01-14 19:54:34 +0530

    Add files via upload

---
+++ InstaSMart/src/app/payment/payment.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit{
+  ngOnInit(): void {}
+  constructor(private router:Router){}
+  onSubmit(pForm:NgForm){
+    alert('Payment Successfull');
+    this.router.navigate(['home']);
+  }
+}
